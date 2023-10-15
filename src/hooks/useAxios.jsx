953f5ff8f8dd0b70f00524631aa5500f7c19fb4e
Shipon Hossen Raju <msshipon234@gmail.com>
2023-10-15 20:37:42 +0600

    firebase deploy test

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const useAxios = () => {
   const Axios = axios.create({
-    baseURL: "http://localhost:5000/",
+    // baseURL: "http://localhost:5000/",
+    baseURL: "https://any-vessel.vercel.app/",
     // baseURL: "https://anyvessel-server.vercel.app/",
   });
   return [Axios];
