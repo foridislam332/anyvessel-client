c28088986a9ebce8cef24d356ca39946b8843a07
radifkhanrafin <meherafkabir@gmail.com>
2023-11-12 12:20:26 +0600

    profile

--- src/Profile/CrewProfile.jsx
+++ src/Profile/CrewProfile.jsx
@@ -2,8 +2,12 @@ import { BiCalendar } from "react-icons/bi";
 import { BsFillFlagFill, BsTelephoneOutbound } from "react-icons/bs";
 import { LuLanguages } from "react-icons/lu";
 import { MdAccountCircle, MdOutlineEmail } from "react-icons/md";
+import CustomModal from "../components/CustomModal";
+import { useState } from "react";
+import useAxios from "../hooks/useAxios";
+import { useForm } from "react-hook-form";
 
-const CrewProfile = ({ user }) => {
+const CrewProfile = ({ user, currentUserLoading, refetch }) => {
   const { surname, email, fullName, gender, phone, role, birthDay } = user;
 
   const calculateAge = (birthDate) => {
@@ -47,6 +51,40 @@ const CrewProfile = ({ user }) => {
 
   const age = calculateAge(birthDay);
 
+  const { register, handleSubmit, watch, setValue, reset, formState: { errors } } = useForm();
+  const [Axios] = useAxios();
+  const [isBasicInfoModalOpen, setIsBasicInfoModalOpen] = useState(false);
+
+
+
+  const handleBasicInfoModal = (e) => {
+    if (e == "cancel") setIsBasicInfoModalOpen(false)
+  }
+
+  const onBasicInfoSubmit = data => {
+    const updateData = {
+      email: email,
+      fullName: data.fullName,
+      // nationality: data.nationality,
+      phone: data.phone,
+      // languages: data.languages,
+      // description: data.description
+    }
+    console.log(updateData)
+    Axios.patch('/crew/basic', updateData)
+      .then(res => {
+        if (res.status === 200) {
+          refetch();
+          setIsBasicInfoModalOpen(false)
+          // reset()
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      })
+
+
+  }
   return (
     <section>
       <div>
@@ -139,9 +177,96 @@ const CrewProfile = ({ user }) => {
         <div className="mt-4 shadow-md rounded-lg p-5">
           <p className="">Unavailable - Found a Crew</p>
         </div>
-
+        <div className="mt-4 shadow-md rounded-lg p-5 flex justify-center">
+          <button className="px-5 py-2 border-2 rounded-md border-purple-600 hover:border-red-800 duration-300" onClick={() => setIsBasicInfoModalOpen(!isBasicInfoModalOpen)}>Update Profile Information</button>
+        </div>
         {/* <p className="  flex items-center">SY - Sailing Yacht (Sloop) , 12.2m(40 ft) , sail , catamarna , <span className="font-semibold flex items-center">Catana <BsBoxArrowUpRight /> 40</span></p> */}
       </div>
+      {
+        isBasicInfoModalOpen &&
+        <CustomModal
+          isModalOpen={isBasicInfoModalOpen}
+          setIsModalOpen={setIsBasicInfoModalOpen}
+          handleModal={handleBasicInfoModal}
+        >
+          <form className='text-black'
+            onSubmit={handleSubmit(onBasicInfoSubmit)}
+          >
+            <h3 className="font-bold text-xl mb-2">Update Your Basic Profile Information</h3>
+            <p className='border-t border-dark mb-5'></p>
+
+            <div className='sm:flex gap-5'>
+              {/* Name */}
+              <div className='w-full'>
+                <label htmlFor="full_name" className='text-dark text-sm'>Name:</label>
+                <input
+                  id="full_name"
+                  {...register("fullName")}
+                  defaultValue={fullName}
+                  placeholder='Your full name'
+                  className='w-full border text-black bg-white border-dark/40 p-2 rounded-md focus:outline-none focus:border-primary mb-1 sm:mb-3'
+                />
+              </div>
+            </div>
+
+            <div className='sm:flex gap-5'>
+              {/* nationality */}
+              {/* <div className='w-full'>
+                <label htmlFor="nationality" className='text-dark text-sm'>Nationality:</label>
+                <input
+                  id="nationality"
+                  {...register("nationality")}
+                  defaultValue={nationality}
+                  placeholder='e.g. Dhaka, Bangladesh'
+                  className='w-full border  text-black bg-white border-dark/40 p-2 rounded-md focus:outline-none focus:border-primary mb-1 sm:mb-3'
+                />
+              </div> */}
+          
+
+              {/* phone */}
+              <div className='w-full'>
+                <label htmlFor="number" className='text-dark text-sm'>Contact Number:</label>
+                <input
+                  id="number"
+                  {...register("phone")}
+                  defaultValue={phone}
+                  placeholder='New Phone Number'
+                  className='w-full border text-black bg-white border-dark/40 p-2 rounded-md focus:outline-none focus:border-primary mb-1 sm:mb-3'
+                />
+              </div>
+            </div>
+            {/* Language */}
+            {/* <div className='w-full'>
+              <label htmlFor="language" className='text-dark text-sm'>Language:</label>
+              <input
+                id="language"
+                {...register("languages")}
+                defaultValue={languages}
+                placeholder='Type Language'
+                className='w-full border  text-black bg-white border-dark/40 p-2 rounded-md focus:outline-none focus:border-primary mb-1 sm:mb-3'
+              />
+            </div> */}
+            {/* About */}
+            {/* <div className='w-full'>
+              <label htmlFor="about" className='text-dark text-sm'>About yourself:</label>
+              <textarea
+                id="about"
+                {...register("description")}
+                defaultValue={description}
+                placeholder='Write about your professional life within 250 words'
+                className='w-full h-32 border text-black bg-white border-dark/40 p-2 rounded-md focus:outline-none focus:border-primary mb-1 sm:mb-3'
+              />
+            </div> */}
+
+            <input
+              className="text-center px-3 md:px-5 py-1 md:py-3 bg-secondary hover:bg-secondary/60 duration-300 rounded-lg text-white mt-2 sm:mt-5 cursor-pointer"
+              type="submit"
+              value="Save Changes"
+            />
+          </form>
+          <p>There is no more data for Update</p>
+        </CustomModal>
+      }
     </section>
   );
 
